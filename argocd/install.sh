a9ce72e33f34ed4ed2e90947d12711f7a148105b
HaruEr <103977717+HaruEr@users.noreply.github.com>
2023-09-04 12:21:53 +0300

    argocd edited (#1)

--- argocd/install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-helm repo add argo https://argoproj.github.io/argo-helm
-helm repo update
-
-kubectl create namespace argocd
-
-helm install argo-cd argo/argo-cd --version 5.43.4 --namespace argocd
